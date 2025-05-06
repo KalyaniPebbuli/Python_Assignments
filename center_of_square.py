58623655b2a47ea81422b482f67bd282e5104ec3
KalyaniPebbuli <kalyani414425@gmail.com>
2025-05-06 16:04:20 +0530

    Add files via upload

---
+++ center_of_square.py
@@ -0,0 +1,7 @@
+rows=int(input("Enter the number of rows: "))
+for i in range(rows):
+    for j in range(rows):
+        if i==j and i==(rows//2) and i==(rows//2):
+            print("*",end="") 
+        else:print(" ",end=" ")
+    print()
